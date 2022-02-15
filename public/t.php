a350945330e06dc2a2005c0782b8b452eb865756
Jack Xia <jackxia5@foxmail.com>
2022-02-15 18:29:37 +0800

    init

---
+++ public/t.php
@@ -0,0 +1,44 @@
+<?php
+// echo 111;
+    phpinfo();
+// ./configure --prefix=/Users/jack/work/tools/php8.1.2 \
+// --with-config-file-path=/Users/jack/work/tools/php8.1.2/etc \
+// --with-config-file-scan-dir=/Users/jack/work/tools/php8.1.2/etc/conf.d \
+// --with-apxs2=/usr/sbin/apxs \
+// --enable-fpm \
+// --with-fpm-user=www \
+// --with-fpm-group=www \
+// --with-mysqli \
+// --with-pdo-mysql \
+// --with-iconv-dir \
+// --with-freetype-dir \
+// --with-zlib \
+// --with-jpeg-dir \
+// --with-png-dir \
+// --with-libxml-dir=/usr/bin/xml2-config \
+// --enable-xml \
+// --disable-rpath \
+// --enable-bcmath \
+// --enable-shmop \
+// --enable-sysvsem \
+// --enable-inline-optimization \
+// --with-curl \
+// --enable-mbregex \
+// --enable-mbstring \
+// --with-mcrypt \
+// --enable-ftp \
+// --with-gd \
+// --enable-gd-native-ttf \
+// --with-openssl \
+// --with-mhash \
+// --enable-pcntl \
+// --enable-sockets \
+// --with-xmlrpc \
+// --enable-zip \
+// --enable-soap \
+// --without-pear \
+// --with-gettext \
+// --disable-fileinfo \
+// --enable-maintainer-zts \
+// --enable-mysqlnd
+?>
